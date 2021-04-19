07cd5f2f0595d996663f0989df27c42b1fe8a0b7
SimoneLeonardiRD <simone.leonardi.rd@gmail.com>
2021-04-19 12:14:32 +0200

    code refactoring

--- reshape_dataset.py
+++ reshape_dataset.py
@@ -5,15 +5,40 @@ fin = open("generated_data/user/users_fake_news.txt", "r")
 for line in fin.readlines():
     users_id.append(line.rstrip("\n"))
 
-users_id = users_id[:10]
 counter = 0
 frames = []
 for user in users_id:
     counter += 1
     try:
-        df = pd.read_csv("generated_data/tweet/"+str(user)+".csv")   
+        df = pd.read_csv("generated_data/tweet/"
+                         + str(user)+".csv", lineterminator='\n')
         df = df.drop(columns=['user_id', 'text'])
         df['label'] = ['1']*df.shape[0]
+        df['user_mapped_id'] = [str(counter)]*df.shape[0]
+        df = df.reindex(columns=['user_mapped_id', 'tweet_id', 'label'])
+        # print(df.head)
+        frames.append(df)
+        print(str(user)+" done "+str(counter)+"\n")
+    except FileNotFoundError:
+        continue
+
+result = pd.concat(frames)
+print(result.shape)
+result.to_csv('coaid_ext_fake.csv', index=False)
+
+users_id = []
+fin = open("generated_data/user/users_real_set.txt", "r")
+for line in fin.readlines():
+    users_id.append(line.rstrip("\n"))
+counter -= 1
+frames = []
+for user in users_id:
+    counter += 1
+    try:
+        df = pd.read_csv("generated_data/tweet_real/"
+                         + str(user)+".csv", lineterminator='\n')
+        df = df.drop(columns=['user_id', 'text'])
+        df['label'] = ['0']*df.shape[0]
         df['user_mapped_id'] = [str(counter)]*df.shape[0]
         df = df.reindex(columns=['user_mapped_id', 'tweet_id', 'label'])
         # print(df.head)
@@ -23,4 +48,11 @@ for user in users_id:
 
 result = pd.concat(frames)
 print(result.shape)
-result.to_csv('coaid_ext_fake.csv', index=False)
+result.to_csv('coaid_ext_real.csv', index=False)
+
+df = pd.read_csv("coaid_ext_fake.csv")
+dr = pd.read_csv("coaid_ext_real.csv")
+frames = [df, dr]
+result = pd.concat(frames)
+print(result.shape)
+result.to_csv('coaid_ext.csv', index=False)
